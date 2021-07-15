43214777da6ba081a366111d20516b0630088949
roel-vereycken <vereyckenroel@gmail.com>
2021-07-15 17:00:36 +0200

    home

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -1,14 +1,32 @@
-import React from 'react'
+import React from "react";
+import { Phone, Mail } from "@material-ui/icons";
 
 export default function Navbar() {
-    return (
-        <div className="navbar">
-            <div className="logo">Roel Vereycken</div>
-            <ul>
-                <li>Home</li>
-                <li>Over mij</li>
-                <li>Contact</li>                
-            </ul>
+  return (
+    <div className="navbar">
+      <div className="left">
+        <div className="logo">
+          <h3>Roel Vereycken</h3>
         </div>
-    )
+        <div className="itemContainer">
+          <div className="card">
+            <Phone className="icon" />
+            <span>+324 71 32 18 55</span>
+          </div>
+          <div className="card">
+            <Mail className="icon" />
+            <span>vereyckenroel@gmail.com</span>
+          </div>
+        </div>
+      </div>
+      <div className="right">
+        <ul>
+          <li>Home</li>
+          <li>Over mij</li>
+          <li>Projecten</li>
+          <li>Contact</li>
+        </ul>
+      </div>
+    </div>
+  );
 }
--- components/Picture.jsx
+++ components/Picture.jsx
@@ -1,9 +1,19 @@
-import React from 'react'
+import React from "react";
 
 export default function Picture() {
-    return (
-        <div className="picture">
-            
+  return (
+    <div className="picture">
+      <div className="card">
+        <div className="text">
+          <h1>Hallo</h1>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Consequatur
+            quibusdam impedit excepturi enim laboriosam deserunt aspernatur
+            voluptatum, iure vitae et.
+          </p>
+          <a href="#about">Over mij</a>
         </div>
-    )
+      </div>
+    </div>
+  );
 }
